05a3bec806c71d4e7f11506b49e06fc3fd78f0c8
Juan Murillo <jg.murillo10@uniandes.edu.co>
2016-08-28 01:25:43 -0500

    CRUD well including atributes in data.js

--- app/routes/api/wells.js
+++ app/routes/api/wells.js
@@ -1,21 +1,111 @@
 var express = require('express');
-
+var wells = require('../../../data').wells;
 var router = express.Router();
 
 // on routes that end in /wells
 // ----------------------------------------------------
 router.route('/')
 
+
+// get all wells
   .get(function(req, res) {
-    res.json('Hello world.');
+    res.json(wells);
   })
 
-  .put(function(req, res) {
-    res.json('Hello world.');
-  })
+// creater new well
+.post(function(req, res){
+	var id = req.body.id;
+	var exists=false;
+	for (var i = 0; i < wells.length; i++) {
+		if(wells[i].id==id){
+			exists=true;
+			break;
+		};
+	}
+	if(!exists){
+		//add well into wells array
+		wells.push({
+
+		'name': req.body.name,
+        'status': req.body.status,
+        'id': req.body.id
+
+		});
+		res.json('Well created. ');
+	}else{
+		res.json('There already is a well with that id');
+	}
+})
+
+router.route('/:well_id')
+
+//get well by id
+
+.get(function(req,res){
+	var id= req.params.tran_id;
+	var i=-1;
+	for (var i = 0; i < wells.length; i++) {
+      if(wells[i].id === id) {
+        break;
+      };
+    }
+
+    if(i !== -1) {
+      res.json(wells[i]);
+    } else {
+      res.json('There is not a well with that id.');
+    }
+})
+
+
+//update a well
+
+.put(function(req,res){
+
+	var id = req.params.tran_id;
+    var i = -1;
+
+    for (var i = 0; i < wells.length; i++) {
+      if(wells[i].id === id) {
+        break;
+      };
+    }
+    
+    if(i !== -1) {
+
+      // update well data
+      if(req.body.name) {
+        wells[i].name = req.body.name;
+      }
+      if(req.body.status) {
+        wells[i].status = req.body.status;
+      }
+
+      res.json('Well updated.');
+    } else {
+      res.json('There is not a well with that id.');
+    }
+	})
+
+
 
   .delete(function(req, res) {
-    res.json('Hello world.');
-  });
+  	var id = req.params.tran_id;
+    var i = -1;
+
+    for (var i = 0; i < wells.length; i++) {
+      if(wells[i].id === id) {
+        break;
+      };
+    }
+    
+    if(i !== -1) {
+      someArray.splice(i,1);
+
+      res.json('Well deleted.');
+    } else {
+      res.json('There is not a well with that id.');
+    }
+  })
 
 module.exports = router;
--- data.js
+++ data.js
@@ -10,5 +10,27 @@ module.exports = {
       'status': 'Jefe de Campo',
       'id': 12345679
     },
+  ],
+  'wells':[
+  {
+    'name': 'Pozo petrolifero 1',
+    'status': 'Abierto',
+    'id': 1
+  },
+  {
+    'name': 'Pozo petrolifero 2',
+    'status': 'Produccion',
+    'id': 2
+  },
+  {
+    'name': 'Pozo petrolifero 3',
+    'status': 'Parado',
+    'id': 3
+  },
+  {
+    'name': 'Pozo petrolifero 4',
+    'status': 'Clausurado',
+    'id': 4
+  }
   ]
 };
