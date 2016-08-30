9681272cf31b19c43921e3b20e1481d16185d420
nonameable <ddelgado2013@gmail.com>
2016-08-30 09:45:10 -0500

    added report.js

---
+++ app/routes/api/reports.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var reports    = require('../../../data').reports;
+var wells = require('../../../data').wells;
+var router = express.Router();
+
+// on routes that end in /reports
+// ----------------------------------------------------
+router.route('/')
+
+
+
+module.exports = router;
