7156542d382df09d25109a9d601674b812217ff1
nonameable <ddelgado2013@gmail.com>
2016-08-30 23:59:24 -0500

    added folder and file for report making

---
+++ app/report_making/reportMaker.js
@@ -0,0 +1,12 @@
+// arrays for validation
+var regions = require('../../types').regions;
+var report_types = require('../../types').report_types;
+var fields = require('../../data').fields;
+var users = require('../../data').users;
+var sensors = require('../../data').sensors;
+var wells = require('../../data').wells;
+module.exports = {
+  
+  
+
+}
