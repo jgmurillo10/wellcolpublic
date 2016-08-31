c1519d761642f4f7bb46616d80398ae73c88767f
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-08-31 17:53:26 -0500

    Refactor Type File

--- types.js
+++ types.js
@@ -1,8 +1,20 @@
 module.exports = {
-	'regions':[
-	'caribe', 'andina', 'pacifica', 'orinoquia', 'amazonas' 
-	],
-	'report_types':[
-	'consumo_energetico', 'produccion_fluido','temperatura'
-	]
+	'regions':{
+    'caribe': 1,
+    'andina': 2,
+    'pacifica': 3,
+    'orinoquia': 4,
+    'amazonas': 5 
+	},
+	'report_types':{
+	  'consumo_energetico': 1,
+    'produccion_fluido': 2,
+    'temperatura': 3
+	},
+  'report_entity':{
+    'region': 1,
+    'field': 2,
+    'well': 3,
+    'sensor': 4,
+  }
 }
