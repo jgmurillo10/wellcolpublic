10f4f39e44dc9ddc5dd29d01bb97b02cec368da9
nonameable <ddelgado2013@gmail.com>
2016-08-31 22:46:37 -0500

    added UPDATE sensor

--- app/routes/api/sensors.js
+++ app/routes/api/sensors.js
@@ -93,6 +93,41 @@ router.route('/:field_id/wells/:well_id/sensors/:sensor_id')
 
   })
 
+  // update a sensor
+  .put(function(req, res){
+    var fieldId = Number(req.params.field_id);
+    var fieldIndex = tools.getFieldIndex(fieldId);
+    if( fieldIndex !== -1){
+      var wellId = Number(req.params.well_id);
+      var wellIndex = tools.getWellIndex(fieldId, wellId);
+      if(wellIndex !== -1){
+        var sensorId = Number(req.params.sensor_id);
+        var sensorIndex = tools.getSensorIndex(fieldId, wellId, sensorId);
+        if(sensorIndex !== -1){
+          var currentSensor = fields[fieldIndex].wells[wellIndex].sensors[sensorIndex];
+          if(req.body.type){
+            currentSensor.type = req.body.type;
+          }
+          if(req.body.rate){
+            currentSensor.rate = req.body.rate;
+          }
+
+          res.json('Sensor updated');
+        } else {
+          res.json('There is no sensor with that id');
+        }
+
+
+      }else {
+        res.json('There is no well with that id');
+      }
+      
+
+    } else {
+      res.json('There is no field with that id');
+    }
+
+  })
     
 
 
