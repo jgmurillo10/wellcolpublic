3008e21de64847a42ad7779b7d7a3f3ecde2c0ce
nonameable <ddelgado2013@gmail.com>
2016-08-31 19:12:35 -0500

    added post for well

--- app/routes/api/wells.js
+++ app/routes/api/wells.js
@@ -11,7 +11,6 @@ router.route('/:field_id/wells')
 
   .get(function(req, res) {
     var fieldId = Number(req.params.field_id);
-    console.log(fieldId);
     var fieldIndex = tools.getIndexField(fieldId);
     if( fieldIndex !== -1){
       res.json(fields[fieldIndex].wells);
@@ -23,124 +22,26 @@ router.route('/:field_id/wells')
 
 // creater new well
 .post(function(req, res){
-	
-
-
-
-  var id = req.body.id;
-	var exists=false;
-	for (var i = 0; i < wells.length; i++) {
-		if(wells[i].id==id){
-			exists=true;
-			break;
-		};
-	}
-	if(!exists){
-		//add well into wells array
-		wells.push({
-
-		'name': req.body.name,
-        'status': req.body.status,
-        'id': req.body.id,
-        'region': req.body.region,
-        'field': req.body.field
-
-		});
-		res.json('Well created. ');
-	}else{
-		res.json('There already is a well with that id');
-	}
-})
-
-router.route('/:well_id')
-
-//get well by id
-
-.get(function(req,res){
-	var id= req.params.well_id;
-	var i=-1;
-	console.log(wells.length);
-	for ( i++; i < wells.length; i++) {
-      if(wells[i].id == id) {
-       	break;
+	var fieldId = Number(req.params.field_id);
+    var fieldIndex = tools.getIndexField(fieldId);
+    if( fieldIndex !== -1){
+      var id  = fields[fieldIndex].wells.length - 1;
+      var newWell = {
+        'id': id,
+        'status' : req.body.status,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'sensors': []
       }
-      else if(i==wells.length-1){
-      	i=-1;
-      	break;
-      }
-    }
-    if(i!=-1)
-    	res.json(wells[i]);
-    else{
 
-    res.json('There is not a well with that id.');
-    	
+      fields[fieldIndex].wells.push(newWell);
+      res.json(newWell);
+    } else {
+      res.json('There is no field with that id');
     }
+
 })
 
 
-//update a well
-
-.put(function(req,res){
-
-	var id = req.params.well_id;
-    var i = -1;
-
-    for (i++; i < wells.length; i++) {
-      if(wells[i].id == id) {
-       	break;
-      }
-      else if(i==wells.length-1){
-      	i=-1;
-      	break;
-      }
-    }
-    
-    if(i !== -1) {
-
-      // update well data
-      if(req.body.name) {
-        wells[i].name = req.body.name;
-      }
-      if(req.body.status) {
-        wells[i].status = req.body.status;
-      }
-      if(req.body.field) {
-        wells[i].field = req.body.field;
-      }
-      if(req.body.region) {
-        wells[i].region  = req.body.region;
-      }
-
-      res.json('Well updated.');
-    } else {
-      res.json('There is not a well with that id.');
-    }
-	})
-
-
-
-  .delete(function(req, res) {
-  	var id = req.params.well_id;
-    var i = -1;
-
-    for (i++; i < wells.length; i++) {
-      if(wells[i].id == id) {
-       	break;
-      }
-      else if(i==wells.length-1){
-      	i=-1;
-      	break;
-      }
-    }
-    
-    if(i !== -1) {
-      wells.splice(i,1);
-
-      res.json('Well deleted.');
-    } else {
-      res.json('There is not a well with that id.');
-    }
-  })
 
 module.exports = router;
