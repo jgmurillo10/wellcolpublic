80a9eba124bff8deeacb7290ed25333f64926cae
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-09-21 19:01:18 -0500

    Cambiar base de datos

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'port': process.env.PORT || 8080,
-  'postgresUrl': 'postgres://xojqteyapsiktf:5Jf7_sd3y0X3bE36IPwOcXGXcE@ec2-50-16-218-45.compute-1.amazonaws.com:5432/dc2k7bp0e40jp0'
+  'postgresUrl': 'postgres://hjccfabtpcbimj:NVvCl-_FXEuGvVlUGESGi9JiXV@ec2-50-17-227-146.compute-1.amazonaws.com:5432/d8in51238c02ca'
 };
