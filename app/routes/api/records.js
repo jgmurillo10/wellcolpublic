94c4c03fdc589f1f645b01bdda10cb9d44fe8fca
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-10-02 20:19:32 -0500

    public folder structure done, with empty files just imports in index.html file

--- app/routes/api/records.js
+++ app/routes/api/records.js
@@ -35,8 +35,19 @@ router.route('/:sensor_id/records')
  // post a sensor's record
     .post(function(req, res) {	
     	var sensorid=Number(req.params.sensor_id);
-    	var value=Number(req.body.temperature.data);
-    	var date=req.body.protime;
+
+if(sensorid==3){
+
+	var value=Number(req.body.temperature.dataTemp);
+	
+}else if(sensorid==18){
+var value=Number(req.body.temperature.dataPot1);
+}else if(sensorid==19){
+var value=Number(req.body.temperature.dataPot2);
+}else if(sensorid==20){
+var value=req.body.temperature.dataPulser;
+}
+	var date=req.body.protime;
     var sql = 'insert into sensor_records (date, sensor_id, value) values ($1, $2, $3) returning *;';
 
     query(sql, [date, sensorid, value], function(err, rows) {
@@ -47,25 +58,10 @@ router.route('/:sensor_id/records')
 
       res.json(response);
     });
+
+    	
   })
-// .post(function(req, res){
 
-//     var sensor_id = Number(req.params.sensor_id);
-//     var params = [
-//     	req.body.date,
-//     	sensor_id,
-//     	req.body.value
-
-//     ];
-//     var sql ='INSERT sensor_records SET (date, sensor_id, value) = ($1, $2, $3) returning *';
-
-//    query(sql,params,function(err,rows){
-//    	if(err) return res.send(err);
-//    	response.message = "record posted";
-//    	response.user = rows[0];
-
-//    	return res.json(respose);
-//    });
 
 
 
