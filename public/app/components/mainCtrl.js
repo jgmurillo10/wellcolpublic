e20932769ce44bda11cd776703290a068e563629
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-10-27 15:23:03 -0500

    Fix for state change by logged in users

--- public/app/components/mainCtrl.js
+++ public/app/components/mainCtrl.js
@@ -8,8 +8,22 @@ angular.module('mainCtrl', [])
   vm.loggedIn = Auth.isLoggedIn();
 
   // check to see if a user is logged in on every request
-  $rootScope.$on('$routeChangeStart', function() {
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
     vm.loggedIn = Auth.isLoggedIn();  
+    // console.log;
+
+    console.log(toState);
+
+    console.log(!vm.loggedIn && toState.name !== 'login');
+    
+    if (toState.name == 'login' && vm.loggedIn) {
+      event.preventDefault();
+      $state.go('home');
+    }
+    else if (toState.name !== 'login' && !vm.loggedIn) {
+      event.preventDefault();
+      $state.go('login');
+    }
 
     // get user information on page load
     Auth.getUser()
@@ -37,7 +51,6 @@ angular.module('mainCtrl', [])
           
         else 
           vm.error = data.message;
-        
       });
   };
 
