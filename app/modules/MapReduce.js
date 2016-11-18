9ecc126c0a09bc14afd9e64d0c3a9f2b4f694d7e
nonameable <ddelgado2013@gmail.com>
2016-11-18 06:43:20 -0500

    mapreduce version

--- app/modules/MapReduce.js
+++ app/modules/MapReduce.js
@@ -1,44 +1,74 @@
 var mapreduce = require('mapred')(); // Leave blank for max performance 
-//var mapreduce = require('mapred')(1); // 1 = single core version (slowest) 
-//var mapreduce = require('mapred')(3); // Use a cluster of 3 processes 
+
+
+//el mapReduce va a funcionar de la siguiente forma
+
+//0. Sacar el tamanio de la tabla records
+//0.1. Partirlo en partes de 50.
+//0.2 sacar los datos y meterlos en un objeto como information. Imagino que es un keyValue
+//2. hacer Map, sumando las repeticiones
+//3. Hacer Reduce juntando los resultados
  
 // Information to process ===================================================== 
- 
-var information = [
-    ['frase primera', 'primer trozo de informacion para procesado primer trozo'],
-    ['segunda frase', 'segundo trozo de informacion trozo de'],
-    ['cacho 3', 'otro trozo para ser procesado otro otro otro trozo'],
-    ['cuarta frase', 'primer trozo de informacion para procesado primer trozo'],
-    ['frase 5', 'segundo trozo de informacion trozo de'],
-    ['sexto cacho', 'otro trozo para ser procesado otro otro otro trozo']
-];
- 
-// User map implementation ===================================================== 
- 
-var map = function(key, value){
-    var list = [], aux = {};
-    value = value.split(' ');
-    value.forEach(function(w){
-        aux[w] = (aux[w] || 0) + 1;
-    });
-    for(var k in aux){
-        list.push([k, aux[k]]);
+
+
+
+
+
+module.exports  = {
+
+
+    // Main function
+
+    getMostFrequentSensor: function (){
+
+        var informationSensors = [
+            ['paq1', '2 3 1 3 2 5 4 3 2 1 1 1 1 2 3 2 2 1 1 3 4'],
+            ['paq2', '2 3 1 4 3 1 4 3 2 1 3 5 1 2 3 2 3 1 1 3 2'],
+            ['paq3', '2 3 1 3 2 5 4 3 2 2 3 1 4 3 1 4 3 1 1 3 4'],
+            ['paq4', '1 4 3 1 1 3 4 3 2 2 3 1 4 3 1 4 3 1 1 3 4'],
+            ['paq5', '1 1 3 4 3 2 2 3 1 3 5 1 2 3 2 4 3 1 2 2 3']
+        ];
+
+        var map = function(key, value) {
+        console.log('value is ' + value);
+        var list = [], aux = {};
+        value = value.split(' ');
+        value.forEach(function(w){
+            aux[w] = (aux[w] || 0) + 1;
+        });
+        for(var k in aux){
+            list.push([k, aux[k]]);
+        }
+        return list;
+        };
+
+        var reduce = function(key, values){
+            var sum = 0;
+            values.forEach(function(e){
+                sum += e;
+            });
+            return sum;
+        };
+
+
+        // MapReduce call ============================================================= 
+
+        mapreduce(informationSensors, map, reduce, function(result){
+            console.log(result);
+            return result;
+        });
     }
-    return list;
+
 };
+
+
+
+
  
-// User reduce implementation ================================================= 
+
+
+
+
  
-var reduce = function(key, values){
-    var sum = 0;
-    values.forEach(function(e){
-        sum += e;
-    });
-    return sum;
-};
  
-// MapReduce call ============================================================= 
- 
-mapreduce(information, map, reduce, function(result){
-    console.log(result);
-});
