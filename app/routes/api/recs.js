0331fd9615a72ed0ee0fac61173736e18f75b333
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2016-11-18 12:17:17 -0500

    POST records donegit add .git add .! gonorrea vida hpta

--- app/routes/api/recs.js
+++ app/routes/api/recs.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+
+var password = 'cuandosientaelboom';
+var input = 'hello world';
 
 
 //funcions for existence validaton
@@ -16,9 +20,53 @@ var query = require('pg-query');
 
 router.route('/')
 .post(function(req,res){
-	var sensor_id=Number(req.body.sensor_id);
-	var date= req.body.date;
-	var value=Number(req.body.value);
+	// var sensor_id=Number(req.body.sensor_id);
+	// var date= req.body.date;
+	// var value=Number(req.body.value);
+
+    console.log('ho');
+	var input=req.body.data;
+
+    console.log(input);
+	//crypto js implementation
+	 // Convert urlsafe base64 to normal base64
+    var input = input.replace(/\-/g, '+').replace(/_/g, '/');
+    // Convert from base64 to binary string
+    var edata = new Buffer(input, 'base64').toString('binary')
+
+    // Create key from password
+    var m = crypto.createHash('md5');
+    m.update(password)
+    var key = m.digest('hex');
+
+    // Create iv from password and key
+    m = crypto.createHash('md5');
+    m.update(password + key)
+    var iv = m.digest('hex');
+
+    // Decipher encrypted data
+    var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv.slice(0,16));
+
+    // UPDATE: crypto changed in v0.10
+    // https://github.com/joyent/node/wiki/Api-changes-between-v0.8-and-v0.10 
+    var nodev = process.version.match(/^v(\d+)\.(\d+)/);
+    var decrypted, plaintext;
+
+    if( nodev[1] === '0' && parseInt(nodev[2]) < 10) {  
+        decrypted = decipher.update(edata, 'binary') + decipher.final('binary');    
+        plaintext = new Buffer(decrypted, 'binary').toString('utf8');
+    } else {
+        plaintext = (decipher.update(edata, 'binary', 'utf8') + decipher.final('utf8'));
+    }
+    console.log('ho');
+    console.log(plaintext);
+    dataarray=plaintext.split("..");
+    var date=dataarray[0];
+    var sensor_id=dataarray[1];
+    var value=dataarray[2];
+    console.log(date+', '+sensor_id+', '+value);
+    //end of crypto js implementation
+
 	// var unit=req.body.unit;
 
 	var sql='insert into sensor_records (date, sensor_id, value) values ($1, $2, $3) returning *;'
--- data/records.py
+++ data/records.py
@@ -1,9 +1,61 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from Crypto.Cipher import AES
+from hashlib import md5
+import base64
 import requests
 import math
 import random
 import json
+
+
+password = 'cuandosientaelboom'
+input = 'hello world'
+
+BLOCK_SIZE = 16
+
+def pad (data):
+    pad = BLOCK_SIZE - len(data) % BLOCK_SIZE
+    return data + pad * chr(pad)
+
+def unpad (padded):
+    pad = ord(padded[-1])
+    return padded[:-pad]
+
+def _encrypt(data, nonce, password):
+    m = md5()
+    m.update(password)
+    key = m.hexdigest()
+
+    m = md5()
+    m.update(password + key)
+    iv = m.hexdigest()
+
+    data = pad(data)
+
+    aes = AES.new(key, AES.MODE_CBC, iv[:16])
+
+    encrypted = aes.encrypt(data)
+    return base64.urlsafe_b64encode(encrypted)
+
+def _decrypt(edata, nonce, password):
+    edata = base64.urlsafe_b64decode(edata)
+
+    m = md5()
+    m.update(password)
+    key = m.hexdigest()
+
+    m = md5()
+    m.update(password + key)
+    iv = m.hexdigest()
+
+    aes = AES.new(key, AES.MODE_CBC, iv[:16])
+    return unpad(aes.decrypt(edata))
+
+output = _encrypt(input, "", password) 
+print(output)
+plaintext = _decrypt(output, "", password)
+print("'" + plaintext + "'")
 
 
 
@@ -16,7 +68,10 @@ import json
 # def postRecord(date, sensor_id, value):
 
 def postRecord(date, ids, val):
-	payload = {'date':date, 'sensor_id':ids, 'value':val}
+	inputData=date+'..'+str(ids)+'..'+str(val)
+	crypt=_encrypt(inputData, "", password)
+	payload = {'data':crypt}
+	print payload
 	url='http://localhost:8080/api/recs'
 	headers = {'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUGVwZHBwcCIsInVzZXJuYW1lIjoicHAxMCIsInR5cGUiOjIsImlhdCI6MTQ3OTQ0NTY4NiwiZXhwIjoxNDc5NjE4NDg2fQ.AMR0gm77hRDGPvZv9EMjQ4TFKpGKTOuJuFhktxxZc0U','Content-Type':'application/json'}
 	r=requests.post(url,data=json.dumps(payload), headers=headers)
@@ -24,8 +79,8 @@ def postRecord(date, ids, val):
 
 
 # postRecord('2005-11-05T08:15:30-05:00', '20','20')
-while (1>0):
-	postRecord('2006-11-05T08:15:30-05:00', random.randint(1, 100), random.randint(15, 25))
+# while (1<0):
+postRecord('2006-11-05T08:15:30-05:00', random.randint(1, 100), random.randint(15, 25))
 
 # postRecord('2006-11-05T08:15:30-05:00', 18,18)
 
